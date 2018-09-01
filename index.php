d67af3fa9b40d7cbc3e0f5c6907fd5acf5d61f5f
Emily Gyles <emg225@cornell.edu>
2018-09-01 15:23:21 -0400

    setting up

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$pageid = "index";
+
+include('includes/init.php');
+include('includes/header.php');
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
+
+  <title>Emily Gyles</title>
+</head>
+
+<body>
